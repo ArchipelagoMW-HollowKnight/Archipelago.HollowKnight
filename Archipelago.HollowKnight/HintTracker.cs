d8f29011305c3d914334643ff9f9ebe900d4dff6
Kono Tyran <HAklowner@gmail.com>
2023-07-29 18:50:39 -0700

    clean up hint-tracker

--- Archipelago.HollowKnight/HintTracker.cs
+++ Archipelago.HollowKnight/HintTracker.cs
@@ -6,6 +6,8 @@ using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.Models;
 using ItemChanger;
 using ItemChanger.Internal;
+using ItemChanger.Placements;
+using Steamworks;
 
 namespace Archipelago.HollowKnight;
 
@@ -24,33 +26,60 @@ public static class HintTracker
     private static void UpdateHints(Hint[] arrayHints)
     {
         Hints = arrayHints.ToList();
-        foreach (var hint in Hints)
+        foreach (Hint hint in Hints)
         {
             if (hint.FindingPlayer != Archipelago.Instance.session.ConnectionInfo.Slot)
                 continue;
 
-            var locationName = StripShopSuffix(_session.Locations.GetLocationNameFromId(hint.LocationId));
-            var location = Finder.GetLocation(locationName);
-            if (location == null)
-                continue;
-            if (!Ref.Settings.Placements.ContainsKey(locationName))
-                continue;
-
-            var placement = Ref.Settings.Placements[locationName];
+            if (!Archipelago.Instance.placementsByLocationID.ContainsKey(hint.LocationId)) continue;
+            AbstractPlacement placement = Archipelago.Instance.placementsByLocationID[hint.LocationId];
 
             if (placement == null)
                 continue;
             
-            //get all items in the placement and set the hinted attribute in ArchipelagoItemTag to true.
-            foreach (var tag in placement.Items.Select(item => item.GetTag<ArchipelagoItemTag>()).Where(tag => tag.Location == hint.LocationId))
+            // set the hinted tag for the single item in the placement that was hinted for.
+            foreach (ArchipelagoItemTag tag in placement.Items.Select(item => item.GetTag<ArchipelagoItemTag>()).Where(tag => tag.Location == hint.LocationId))
             {
                 tag.Hinted = true;
             }
             
-            //if there is only 1 item in this placement mark the whole placement as visited.
-            if (placement.Items.Count > 1) continue;
-            placement.GetTag<ArchipelagoPlacementTag>().Hinted = true;
-            placement.AddVisitFlag(VisitState.Previewed);
+            // if all items inside a placement have been hinted for then mark the entire placement as hinted.
+            if (placement.Items.TrueForAll(item => item.GetTag<ArchipelagoItemTag>().Hinted))
+            {
+                placement.GetTag<ArchipelagoPlacementTag>().Hinted = true;
+            }
+
+            if (placement is ShopPlacement)
+            {
+                List<(string, AbstractItem)> previewText = new();
+                ShopPlacement shop = (ShopPlacement) placement;
+                foreach (AbstractItem item in shop.Items)
+                {
+                    if (item.GetTag<ArchipelagoItemTag>().Hinted)
+                    {
+                        previewText.Add((item.GetPreviewWithCost(), item));
+                    }
+                    else
+                    {
+                        previewText.Add((Language.Language.Get("???", "IC"), item));
+                    }
+                    
+                }
+                shop.OnPreviewBatch(previewText);
+            }
+            else
+            {
+                List<string> previewText = new();
+                foreach (AbstractItem item in placement.Items)
+                {
+                    previewText.Add(item.GetTag<ArchipelagoItemTag>().Hinted
+                        ? item.GetPreviewWithCost()
+                        : Language.Language.Get("???", "IC"));
+                }
+                
+                placement.OnPreview(string.Join(", ", previewText));
+            }
+            
         }
         
         try
@@ -61,30 +90,6 @@ public static class HintTracker
         {
             Archipelago.Instance.LogError($"Error invoking OnArchipelagoHintUpdate:\n {ex}");
         }
-    }
-    
-    private static string StripShopSuffix(string location)
-    {
-        if (string.IsNullOrEmpty(location))
-        {
-            return null;
-        }
-
-        var names = new[]
-        {
-            LocationNames.Sly_Key, LocationNames.Sly, LocationNames.Iselda, LocationNames.Salubra,
-            LocationNames.Leg_Eater, LocationNames.Egg_Shop, LocationNames.Seer, LocationNames.Grubfather
-        };
-
-        foreach (var name in names)
-        {
-            if (location.StartsWith(name))
-            {
-                return location.Substring(0, name.Length);
-            }
-        }
-
-        return location;
     }
 
     public static void Start(ArchipelagoSession archipelagoSession)
