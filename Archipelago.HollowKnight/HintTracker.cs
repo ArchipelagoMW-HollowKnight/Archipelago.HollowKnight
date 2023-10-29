f9d6bf7c2300849fa10e34beb93f38ca311cd480
Sean Dempsey <dempsey.sean@outlook.com>
2023-10-29 14:57:37 -0700

    Add soul totem and geo rock subtype tags to placements for remote hk items
    Resolves #193

--- Archipelago.HollowKnight/HintTracker.cs
+++ Archipelago.HollowKnight/HintTracker.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Archipelago.HollowKnight.IC;
+﻿using Archipelago.HollowKnight.IC;
 using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.Models;
 using ItemChanger;
 using ItemChanger.Modules;
 using ItemChanger.Placements;
 using ItemChanger.Tags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Archipelago.HollowKnight;
 
@@ -29,13 +29,21 @@ public class HintTracker : Module
         foreach (Hint hint in Hints)
         {
             if (hint.FindingPlayer != Archipelago.Instance.session.ConnectionInfo.Slot)
+            {
                 continue;
+            }
 
-            if (!Archipelago.Instance.placementsByLocationID.ContainsKey(hint.LocationId)) continue;
+            if (!Archipelago.Instance.placementsByLocationID.ContainsKey(hint.LocationId))
+            {
+                continue;
+            }
+
             AbstractPlacement placement = Archipelago.Instance.placementsByLocationID[hint.LocationId];
 
             if (placement == null)
+            {
                 continue;
+            }
 
             // set the hinted tag for the single item in the placement that was hinted for.
             foreach (ArchipelagoItemTag tag in placement.Items.Select(item => item.GetTag<ArchipelagoItemTag>())
@@ -73,7 +81,9 @@ public class HintTracker : Module
                     string str = p.Item1;
                     int index = shop.Items.IndexOf(p.item);
                     if (index >= 0)
+                    {
                         previewRecordTag.previewTexts[index] = str;
+                    }
                 }
             }
             else
@@ -81,7 +91,11 @@ public class HintTracker : Module
                 List<string> previewText = new();
                 foreach (AbstractItem item in placement.Items)
                 {
-                    if(item.WasEverObtained()) continue;
+                    if(item.WasEverObtained())
+                    {
+                        continue;
+                    }
+
                     previewText.Add(item.GetTag<ArchipelagoItemTag>().Hinted
                         ? item.GetPreviewWithCost()
                         : Language.Language.Get("???", "IC"));
@@ -117,7 +131,7 @@ public class HintTracker : Module
 
     private void UpdateHintFoundStatus(ReadOnlyGiveEventArgs args)
     {
-        if (args.Orig.GetTag(out ArchipelagoItemTag tag))
+        if (Hints != null && args.Orig.GetTag(out ArchipelagoItemTag tag))
         {
             long location = tag.Location;
             foreach (Hint hint in Hints)
