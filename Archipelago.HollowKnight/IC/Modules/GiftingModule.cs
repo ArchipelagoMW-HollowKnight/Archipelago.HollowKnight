4f4bfc2387064238198955c2b352c2db2180d82b
Sean Dempsey <dempsey.sean@outlook.com>
2025-02-02 10:51:40 -0800

    Update to gifting protocol v3

--- Archipelago.HollowKnight/IC/Modules/GiftingModule.cs
+++ Archipelago.HollowKnight/IC/Modules/GiftingModule.cs
@@ -1,12 +1,11 @@
-﻿using Archipelago.Gifting.Net.Gifts.Versions.Current;
-using Archipelago.Gifting.Net.Service;
+﻿using Archipelago.Gifting.Net.Service;
 using Archipelago.Gifting.Net.Traits;
+using Archipelago.Gifting.Net.Versioning.Gifts.Current;
 using Archipelago.MultiClient.Net;
 using ItemChanger;
 using ItemChanger.Modules;
 using ItemChanger.Tags;
 using MenuChanger;
-using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -32,8 +31,8 @@ public class GiftingModule : Module
     {
         if (giftingService != null)
         {
-            session.DataStorage[giftingService.GetMyGiftBoxKey()].OnValueChanged -= OnGiftBoxUpdated;
             giftingService.CloseGiftBox();
+            giftingService.OnNewGift -= ReceiveOrRefundGift;
             giftingService = null;
         }
     }
@@ -42,101 +41,85 @@ public class GiftingModule : Module
     {
         orig(self);
         On.GameManager.FinishedEnteringScene -= BeginGifting;
-        session.DataStorage[giftingService.GetMyGiftBoxKey()].OnValueChanged += OnGiftBoxUpdated;
+        giftingService.OnNewGift += ReceiveOrRefundGift;
         Dictionary<string, Gift> pendingGifts = await giftingService.CheckGiftBoxAsync();
-        ReceiveOrRefundGifts(pendingGifts.Values);
+        foreach (Gift gift in pendingGifts.Values)
+        {
+            ReceiveOrRefundGift(gift);
+        }
         giftingService.OpenGiftBox(false, AcceptedTraits);
     }
 
-    private void OnGiftBoxUpdated(JToken originalValue, JToken newValue, Dictionary<string, JToken> additionalArguments)
+    private void ReceiveOrRefundGift(Gift gift)
     {
-        // todo reshape the token into gifts and process. Also check the diff of old vs new gifts
-        Dictionary<string, Gift> oldGifts = originalValue.ToObject<Dictionary<string, Gift>>();
-        Dictionary<string, Gift> newGifts = newValue.ToObject<Dictionary<string, Gift>>();
-        foreach (string key in oldGifts.Keys)
+        giftingService.RemoveGiftFromGiftBox(gift.ID);
+        GiftTrait bestTrait = PickBestMatchingTrait(gift);
+        if (bestTrait != null)
         {
-            // gifts still in the giftbox which have not yet been processed, ignore them
-            newGifts.Remove(key);
-        }
-        if (newGifts.Count > 0)
-        {
-            ReceiveOrRefundGifts(newGifts.Values);
-        }
-    }
-
-    private void ReceiveOrRefundGifts(IEnumerable<Gift> gifts)
-    {
-        foreach (Gift gift in gifts)
-        {
-            giftingService.RemoveGiftFromGiftBox(gift.ID);
-            GiftTrait bestTrait = PickBestMatchingTrait(gift);
-            if (bestTrait != null)
+            string itemName;
+            if (bestTrait.Trait == GiftFlag.Mana)
             {
-                string itemName;
-                if (bestTrait.Trait == GiftFlag.Mana)
+                // soul refill based on quality. Average is 90 (large totems), below is 54, above is 200, scaling linearly
+                if (bestTrait.Quality >= 2.2)
                 {
-                    // soul refill based on quality. Average is 90 (large totems), below is 54, above is 200, scaling linearly
-                    if (bestTrait.Quality >= 2.2)
-                    {
-                        itemName = ItemNames.Soul_Totem_Path_of_Pain;
-                    }
-                    else if (bestTrait.Quality <= 0.6)
-                    {
-                        itemName = ItemNames.Soul_Totem_B;
-                    }
-                    else
-                    {
-                        itemName = ItemNames.Soul_Totem_A;
-                    }
+                    itemName = ItemNames.Soul_Totem_Path_of_Pain;
                 }
-                else if (bestTrait.Trait == GiftFlag.Life)
+                else if (bestTrait.Quality <= 0.6)
                 {
-                    // blue hearts based on quality. Average is 2 blue masks, scaling linearly from that
-                    // todo - do we want XL/XS lifeblood for more interesting variance?
-                    if (bestTrait.Quality >= 1.5)
-                    {
-                        itemName = ItemNames.Lifeblood_Cocoon_Large;
-                    }
-                    else
-                    {
-                        itemName = ItemNames.Lifeblood_Cocoon_Small;
-                    }
-                }
-                else if (bestTrait.Trait == "Artifact")
-                {
-                    // relic based on quality (average case is Hallownest seal, scales roughly linearly from that)
-                    if (bestTrait.Quality <= 0.4)
-                    {
-                        itemName = ItemNames.Wanderers_Journal;
-                    }
-                    else if (bestTrait.Quality >= 2.7)
-                    {
-                        itemName = ItemNames.Arcane_Egg;
-                    }
-                    else if (bestTrait.Quality >= 1.8)
-                    {
-                        itemName = ItemNames.Kings_Idol;
-                    }
-                    else
-                    {
-                        itemName = ItemNames.Hallownest_Seal;
-                    }
+                    itemName = ItemNames.Soul_Totem_B;
                 }
                 else
                 {
-                    // safety net in case we update acceptedtraits
-                    ArchipelagoMod.Instance.LogWarn($"Got an unexpected trait {bestTrait} for gift {gift}");
-                    giftingService.RefundGift(gift);
-                    continue;
+                    itemName = ItemNames.Soul_Totem_A;
                 }
-
-                string sender = session.Players.GetPlayerName(gift.SenderSlot);
-                DispatchItem(itemName, gift.Amount, sender);
+            }
+            else if (bestTrait.Trait == GiftFlag.Life)
+            {
+                // blue hearts based on quality. Average is 2 blue masks, scaling linearly from that
+                // todo - do we want XL/XS lifeblood for more interesting variance?
+                if (bestTrait.Quality >= 1.5)
+                {
+                    itemName = ItemNames.Lifeblood_Cocoon_Large;
+                }
+                else
+                {
+                    itemName = ItemNames.Lifeblood_Cocoon_Small;
+                }
+            }
+            else if (bestTrait.Trait == "Artifact")
+            {
+                // relic based on quality (average case is Hallownest seal, scales roughly linearly from that)
+                if (bestTrait.Quality <= 0.4)
+                {
+                    itemName = ItemNames.Wanderers_Journal;
+                }
+                else if (bestTrait.Quality >= 2.7)
+                {
+                    itemName = ItemNames.Arcane_Egg;
+                }
+                else if (bestTrait.Quality >= 1.8)
+                {
+                    itemName = ItemNames.Kings_Idol;
+                }
+                else
+                {
+                    itemName = ItemNames.Hallownest_Seal;
+                }
             }
             else
             {
+                // safety net in case we update acceptedtraits
+                ArchipelagoMod.Instance.LogWarn($"Got an unexpected trait {bestTrait} for gift {gift}");
                 giftingService.RefundGift(gift);
+                return;
             }
+
+            string sender = session.Players.GetPlayerName(gift.SenderSlot);
+            DispatchItem(itemName, gift.Amount, sender);
+        }
+        else
+        {
+            giftingService.RefundGift(gift);
         }
     }
 
